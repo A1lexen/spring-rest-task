6c3d3460c9cd2d79aad7fa1c7a1af218253ffa20
Stas Kachurovskiy <larevoo@gmail.com>
2021-07-01 14:04:01 +0300

    spring boot was initialized

---
+++ src/test/java/co/inventorsoft/spring_rest_task/SpringRestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package co.inventorsoft.spring_rest_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
