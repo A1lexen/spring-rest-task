6844bce05d3873dcc0f7cc2afc34f1af95d05b10
Vlad Zhloba <zhloba96@gmail.com>
2021-11-24 21:12:40 +0200

    adaptation for rest

--- src/main/java/com/example/springresttask/controller/FileController.java
+++ src/main/java/com/example/springresttask/controller/FileController.java
@@ -5,20 +5,14 @@ import com.example.springresttask.service.FileService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 
-@Controller
+@RestController
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
 @RequestMapping("/files")
@@ -26,53 +20,24 @@ public class FileController {
     FileService fileService;
 
     @GetMapping
-    public String files(Model model) {
-        List<UploadedFile> fileList = fileService.getFileList();
-        model.addAttribute("files", fileList);
-        return "/files";
+    public List<UploadedFile> files() {
+        return fileService.getFileList();
     }
 
-    @GetMapping("/upload")
-    public String upload() {
-        return "/upload";
-    }
-
-    @GetMapping("/download/{id}")
-    public String downloadFile(@PathVariable Integer id) {
-        fileService.downloadFile(id);
-        return "redirect:/files";
-    }
-
-    @GetMapping("/delete")
-    public String delete(Model model) {
-        model.addAttribute("files", fileService.getFileList());
-        return "/delete";
-    }
-
-
-    @GetMapping("/update")
-    public String update(Model model) {
-        model.addAttribute("files", fileService.getFileList());
-        return "/update";
-    }
-
-    @PostMapping
-    public String addFile(@RequestParam("file") MultipartFile file) {
-        fileService.addFileToList(file);
-        return "redirect:/files";
+    @PostMapping("/upload")
+    public ResponseEntity uploadFile(@RequestParam("file") MultipartFile file) {
+        return fileService.uploadFile(file);
     }
 
     @PostMapping("/update")
-    public String updateFile(@RequestParam("newFileName") String newFileName,
-                             @RequestParam("oldFileName") String oldFileName) {
-        fileService.updateFile(newFileName, oldFileName);
-        return "redirect:/";
+    public ResponseEntity updateFile(@RequestParam("newFileName") String newFileName,
+                                     @RequestParam("oldFileName") String oldFileName) {
+        return fileService.updateFile(newFileName, oldFileName);
     }
 
     @PostMapping("/delete")
-    public String deleteFile(@RequestParam("fileName") String fileName) {
-        fileService.deleteFile(fileName);
-        return "redirect:/";
+    public ResponseEntity deleteFile(@RequestParam("fileName") String fileName) {
+        return fileService.deleteFile(fileName);
     }
 
 }
