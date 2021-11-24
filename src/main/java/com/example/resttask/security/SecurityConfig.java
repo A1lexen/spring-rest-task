5fe99fe6ea8d53800cce8e718a0da78c174c4f9b
julia <jkhomyn1381@gmail.com>
2021-11-25 01:05:27 +0200

    added security

---
+++ src/main/java/com/example/resttask/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.resttask.security;
+
+import com.example.resttask.model.Role;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.DELETE).hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.POST).hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.GET).hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+                .anyRequest().authenticated().and().httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .roles(Role.ADMIN.name())
+                        .build(),
+                User.builder()
+                        .username("user")
+                        .password(passwordEncoder.encode("user"))
+                        .roles(Role.USER.name())
+                        .build()
+        );
+    }
+
+}
